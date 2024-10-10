f8f2397a4e280d2ffe0452cc873b1895e3d5cbac
Ranyere <ranyereramiresg@hotmail.com>
2024-10-10 13:45:32 -0300

    Iniciando padrão de uso do dapper no projeto

--- Sql/Repositories/EnderecoRepository.cs
+++ Sql/Repositories/EnderecoRepository.cs
@@ -1,6 +1,47 @@
-﻿namespace Agenda_Manager.Sql.Repositories
+﻿using Agenda_Manager.Models.Models;
+using Dapper;
+using MySqlConnector;
+
+namespace Agenda_Manager.Sql.Repositories
 {
     public class EnderecoRepository
     {
+        private readonly string connectionString;
+
+        public EnderecoRepository(IConfiguration configuration)
+        {
+            connectionString = configuration["MySql:ConnectionString"];
+        }
+
+        public async Task Add(EnderecoModel endereco)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var transaction = await connection.BeginTransactionAsync())
+                {
+                    try
+                    {
+                        var sql = @"";
+
+                        var parameters = new DynamicParameters();
+
+                        await connection.ExecuteAsync(sql, parameters, transaction);
+
+                        await transaction.CommitAsync();
+                    }
+                    catch (Exception)
+                    {
+                        await transaction.RollbackAsync();
+                        throw;
+                    }
+                    finally
+                    {
+                        await connection.CloseAsync();
+                    }
+                }
+            }
+        }
     }
 }
