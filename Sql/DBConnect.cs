e2f58bf608e764256f30d43b75a060934689eeec
Ranyere <ranyereramiresg@hotmail.com>
2024-10-10 13:29:44 -0300

    configuração de banco de dados

---
+++ Sql/DBConnect.cs
@@ -0,0 +1,16 @@
+﻿namespace Agenda_Manager.Sql
+{
+    public class DBConnect
+    {
+        public string GetConnectionString(IConfiguration configuration)
+        {
+            var host = configuration["MySql:Host"];
+            var port = configuration["MySql:Port"];
+            var userName = configuration["MySql:UserName"];
+            var password = configuration["MySql:Password"];
+            var database = configuration["MySql:DataBase"];
+
+            return $"Server={host};Port={port};User Id={userName};Password={password};Database={database};";
+        }
+    }
+}
